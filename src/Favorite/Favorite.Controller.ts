576375319b7683b3550c2b7f45ce5c6b7eaac2f3
Brant <brantrusnakx95@gmail.com>
2020-04-16 12:31:27 -0400

    Fixed getFavorites, gets post and userDetails.

--- src/Favorite/Favorite.Controller.ts
+++ src/Favorite/Favorite.Controller.ts
@@ -5,59 +5,59 @@ import { UserDetails } from '../UserDetails/UserDetails.Model';
 
 class FavoriteController {
   public async favorite(req: Request, res: Response) {
-    let user = (req.user as {id: number});
+    let user = (req.user as { id: number });
     let favoriteCheck = await Favorite.findOne({
       where: { userId: user.id, postId: req.params.postId }
     });
     if (!favoriteCheck) {
       Favorite.create({ userId: user.id, postId: req.params.postId });
-      Post.increment('favoriteCount', {by: 1, where: {id: req.params.postId}})
-      res.status(200).send({message: 'Favorited post'});
+      Post.increment('favoriteCount', { by: 1, where: { id: req.params.postId } })
+      res.status(200).send({ message: 'Favorited post' });
     } else {
-      res.status(400).send({message: 'Already favorited'});
+      res.status(400).send({ message: 'Already favorited' });
     }
   }
 
   public async unfavorite(req: Request, res: Response) {
-    let user = (req.user as {id: number});
+    let user = (req.user as { id: number });
     let favoriteCheck = await Favorite.findOne({
       where: { userId: user.id, postId: req.params.postId }
     });
     if (favoriteCheck) {
       // Check if favorite exists
-      Favorite.destroy({where: { userId: user.id, postId: req.params.postId }});
-      Post.increment('favoriteCount', {by: -1, where: {id: req.params.postId}})
-      res.status(200).send({message: 'Unfavorited post'});
+      Favorite.destroy({ where: { userId: user.id, postId: req.params.postId } });
+      Post.increment('favoriteCount', { by: -1, where: { id: req.params.postId } })
+      res.status(200).send({ message: 'Unfavorited post' });
     } else {
-      res.status(400).send({message: 'Post is not favored'});
+      res.status(400).send({ message: 'Post is not favored' });
     }
   }
 
   public async getFavorites(req: Request, res: Response) {
-    let user = (req.user as {id: number});
+    let user = (req.user as { id: number });
     let favorites = await Favorite.findAll({
       where: {
         userId: user.id
       }
     })
-    let favoriteIds = [];
-    for(let favorite of favorites) {
-      favoriteIds.push(favorite.postId)
-    }
-    // Need to think about returning posts in order(favorite time vs post time).
-    let posts = await Post.findAll({
-      where: {
-        id: favoriteIds
-      },
-      include: [{
-        model: UserDetails,
-        as: "userDetails",
+    let posts = [];
+    for (let favorite of favorites) {
+      let post = await Post.findOne({
         where: {
-          userId: favoriteIds
-        }
-      }]
-    })
-    res.status(200).send({posts});
+          id: favorite.postId
+        },
+        include: [{
+          model: UserDetails,
+          as: "userDetails",
+          where: {
+            userId: favorite.userId
+          }
+        }]
+      })
+      console.log(post);
+      posts.push(post);
+    }
+    res.status(200).send({ posts });
   }
 
 }
