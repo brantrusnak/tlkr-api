a93c051ad518ab580aeb7cb865b571a6ff5a3cda
Brant <brantrusnakx95@gmail.com>
2020-04-18 20:25:02 -0400

    Updated build/start for node.

--- src/Database/Database.ts
+++ src/Database/Database.ts
@@ -26,8 +26,6 @@ export class Database {
       acquire: parseInt(process.env.DATABASE_POOL_ACQUIRE),
       idle: parseInt(process.env.DATABASE_POOL_IDLE)
     },
-    operatorsAliases: {
-    },
     logging: process.env.DATABASE_LOGGING === 'true'
   });
 
